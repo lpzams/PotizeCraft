bc2ca232f3f58c9111921a31ee48e337a0c69b72
lapz <lapz_2023@qq.com>
2025-05-09 13:34:24 +0800

    Initial commit

---
+++ poetize-server/poetry-web/src/main/java/com/ld/poetry/config/SwaggerConfig.java
@@ -0,0 +1,29 @@
+package com.ld.poetry.config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springdoc.core.GroupedOpenApi;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI apiInfo() {
+        return new OpenAPI()
+                .info(new Info()
+                        .title("Poetry Web API")
+                        .description("最美博客的后台接口文档")
+                        .version("v1.0"));
+    }
+
+    // 可选：定义多个分组
+    @Bean
+    public GroupedOpenApi publicApi() {
+        return GroupedOpenApi.builder()
+                .group("public-api")
+                .pathsToMatch("/**")
+                .build();
+    }
+}
